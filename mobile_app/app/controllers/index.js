6e4b4b77dac924039773f48857d27bc6e07a7e67
Adrian Vladu <adyv_adyv@yahoo.com>
2013-11-09 05:57:12 +0000

    Added network request

--- mobile_app/app/controllers/index.js
+++ mobile_app/app/controllers/index.js
@@ -1,1 +1,19 @@
 $.index.open();
+ var url = "ec2-54-220-99-234.eu-west-1.compute.amazonaws.com:3000";
+ var client = Ti.Network.createHTTPClient({
+     // function called when the response data is available
+     onload : function(e) {
+         Ti.API.info("Received text: " + this.responseText);
+         alert(this.responseText);
+     },
+     // function called when an error occurs, including a timeout
+     onerror : function(e) {
+         Ti.API.debug(e.error);
+         alert('error');
+     },
+     timeout : 5000  // in milliseconds
+ });
+ // Prepare the connection.
+ client.open("GET", url);
+ // Send the request.
+ client.send();
