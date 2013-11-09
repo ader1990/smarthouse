f1a5cb3f873a65ff89539e4da2d3a36844dc978f
mcoder33 <costineanpaul@gmail.com>
2013-11-09 04:27:45 +0200

    nodejs basic clustering solved test please and siege

--- server_app/server.js
+++ server_app/server.js
@@ -1,25 +1,38 @@
-var express = require('express');
-var app = express();
+
 var cluster = require('cluster');
 
 if (cluster.isMaster)
 	{
 	console.log('Master is online. Spawning workers.')
-	var cpuCount = require('os').cpus().length;
-	for(var i = 0; i < cpuCount; i++)
+	
+	var cpuNumber = require('os').cpus().length;
+	for(var i = 0; i < cpuNumber; i++)
+	
 	{
 		cluster.fork();
 		console.log('Worker #' + i + ' spawned.');
 	}
 
-
-//Express configuration
-require('./config.js')(app);
-
-//API Routes
-require('./routes/route_api.js')(app);
+	cluster.on('exit', function(worker) {
+		console.log('Worker #' + worker.id + ' died :(');
+	});
 }
+else
+{
+	var express = require('express');
+	
+	var app = express();
 
-console.log('Worker #' + cluster.worker.id + ' listening on port: 3000');
+		//Express configuration
+	require('./config.js')(app);
 
-app.listen(3000);
+	//API routes
+	//require('./routes/route_api.js')(app);
+	app.get('/', function(req, res){
+		res.send("Worker #" + cluster.worker.id + " served this webpage.");
+	});
+
+	console.log('Worker #' + cluster.worker.id + ' listening on port: 3000');
+	
+	app.listen(3000);	
+}
