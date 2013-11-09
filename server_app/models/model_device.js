fe4397776b9be609d04197671de16330335b398d
treebirg1 <treebirg@gmail.com>
2013-11-09 10:42:42 +0200

    Changed phone_nr to user_id

--- server_app/models/model_device.js
+++ server_app/models/model_device.js
@@ -1,29 +1,31 @@
+
+
 exports.get_info = function(params,cb){
-	db.collection('devices').findOne({'phone_nr':params.phone_nr},function(err,dev_doc){
+	db.collection('devices').findOne({'user_id':params.phone_nr},function(err,dev_doc){
 		if(err) cb(err,null);
 		else{
-			if(dev_doc) cb(null,200);
+			if(dev_doc) cb(null,dev_doc);
 			else cb(null,404);
 		}
 	});
 };
 
 exports.get_homes = function(params,cb){
-	db.collection('devices').findOne({'phone_nr':params.phone_nr},function(err,dev_doc){
+	db.collection('devices').findOne({'user_id':params.phone_nr},function(err,dev_doc){
 		if(err) cb(err,null);
 		else cb(null,dev_doc.homes);
 	});
 };
 
 exports.delete_home = function(params,cb){
-	db.collection('devices').update({'phone_nr':params.phone_nr},{$pull:{'homes':{'home_id':params.home_id}}},{safe:true},function(err,dev_doc){
+	db.collection('devices').update({'user_id':params.phone_nr},{$pull:{'homes':{'home_id':params.home_id}}},{safe:true},function(err,dev_doc){
 		if(err) cb(err,null);
 		else cb(null,dev_doc);
 	});
 };
 
 exports.add_home = function(params,cb){
-	db.collection('devices').update({'phone_nr':params.phone_nr},{$push:{'homes':params.home}},{safe:true},function(err,dev_doc){
+	db.collection('devices').update({'user_id':params.phone_nr},{$push:{'homes':params.home}},{safe:true},function(err,dev_doc){
 		if(err) cb(err,null);
 		else cb(null,err);
 	});
--- server_app/routes/route_api.js
+++ server_app/routes/route_api.js
@@ -8,10 +8,11 @@ module.exports = function(db,app){
 	app.get('/', function(req, res){
 		res.send('hello world');
 	});
+	
 	app.post('/', function(req,res){
 		Test.testAdd(db,req.body,function(err,status){
 			if(err) console.log(err);
-			else res.send('success! check console :D');
+			else res.send(200);
 		});
 	});
 	
