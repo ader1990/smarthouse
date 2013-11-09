3a7d2c8e50c21eaf930c0feea00617616ebdc00d
mcoder33 <costineanpaul@gmail.com>
2013-11-09 03:50:34 +0200

    clustering and sh1t

--- server_app/server.js
+++ server_app/server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
+var cluster = require('cluster');
+
+if (cluster.isMaster)
+	{
+	console.log('Master is online. Spawning workers.')
+	var cpuCount = require('os').cpus().length;
+	for(var i = 0; i < cpuCount; i++)
+	{
+		cluster.fork();
+		console.log('Worker #' + i + ' spawned.');
+	}
+
 
 //Express configuration
 require('./config.js')(app);
 
 //API Routes
 require('./routes/route_api.js')(app);
+}
 
-console.log('Server listening on port: 3000');
+console.log('Worker #' + cluster.worker.id + ' listening on port: 3000');
 
 app.listen(3000);
